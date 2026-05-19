8422184e252ae83a3d8f5d9758794e31ab5e284b
machtendo <machjas@outlook.com>
2026-05-18 22:50:13 -0500

    refactor configuration

--- hosts/mediarr/configuration.nix
+++ hosts/mediarr/configuration.nix
@@ -8,6 +8,7 @@
     # import any other modules here
     imports = [
       self.nixosModules.mediarrHardware
+      self.nixosModules.baseConfiguration
     ];
 
     # Enable Flakes
@@ -19,7 +20,6 @@
     boot.loader.grub.useOSProber = true;
 
     networking.hostName = "nixos"; # Define your hostname.
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
@@ -27,30 +27,6 @@
 
     # Enable networking
     networking.networkmanager.enable = true;
-
-    # Set your time zone.
-    # time.timeZone = "America/Chicago";
-    #
-    # Select internationalisation properties.
-    # i18n.defaultLocale = "en_US.UTF-8";
-    #
-    # i18n.extraLocaleSettings = {
-    #   LC_ADDRESS = "en_US.UTF-8";
-    #   LC_IDENTIFICATION = "en_US.UTF-8";
-    #   LC_MEASUREMENT = "en_US.UTF-8";
-    #   LC_MONETARY = "en_US.UTF-8";
-    #   LC_NAME = "en_US.UTF-8";
-    #   LC_NUMERIC = "en_US.UTF-8";
-    #   LC_PAPER = "en_US.UTF-8";
-    #   LC_TELEPHONE = "en_US.UTF-8";
-    #   LC_TIME = "en_US.UTF-8";
-    # };
-    #
-    # Configure keymap in X11
-    # services.xserver.xkb = {
-    #   layout = "us";
-    #   variant = "";
-    # };
 
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.nix = {
@@ -60,11 +36,7 @@
       packages = with pkgs; [];
     };
 
-    # Allow unfree packages
-    #nixpkgs.config.allowUnfree = true;
-
     # List packages installed in system profile. To search, run:
-    # $ nix search wget
     environment.systemPackages = with pkgs; [
       neovim        # Neovim - Text Editor
       wget
@@ -72,9 +44,7 @@
     ];
 
     # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
+    services.openssh.enable = true;     # OpenSSH Server
 
     # Open ports in the firewall.
     # networking.firewall.allowedTCPPorts = [ ... ];
