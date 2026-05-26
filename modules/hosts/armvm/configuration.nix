e2ff9af5f0f7d0b6db779e5895dfe0f692607c29
machtendo <machjas@outlook.com>
2026-05-26 13:54:19 -0500

    New Host: armvm - Parallels VM

---
+++ modules/hosts/armvm/configuration.nix
@@ -0,0 +1,63 @@
+#---------------------------------------------------------------------------------------------------
+# Host Configuration: armvm
+#---------------------------------------------------------------------------------------------------
+
+{ self, inputs, ... }: {
+
+  flake.nixosConfigurations.armvm = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.hostArmvm
+    ];
+  };
+
+  flake.nixosModules.hostArmvm = { pkgs, ... }: {
+    # Import Modules
+    imports = [
+      # Host Configuration
+      self.nixosModules.baseConfiguration
+      self.nixosModules.armvmHardware
+
+      # Modules
+
+    ];
+
+    system.stateVersion = "25.11";
+
+    # Bootloader.
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
+
+    # Networking ---------------------------------
+
+    # Enable Networking
+    networking.networkmanager.enable = true;
+
+    # Hostname
+    networking.hostName = "armvm";
+
+    # Network Proxy
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Firewall
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    # Packages  ----------------------------------
+
+    # Packages (System)
+    environment.systemPackages = with pkgs; [
+     # ...
+    ];
+
+    # Services
+    services.openssh.enable = true;     # OpenSSH Server
+  };
+}
+
+#---------------------------------------------------------------------------------------------------
+# End
+#---------------------------------------------------------------------------------------------------
