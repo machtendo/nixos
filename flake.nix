5e55e588bdf7d46abc24e107a650d11769193619
machtendo <machjas@outlook.com>
2026-07-05 23:38:35 -0500

    comments on imports

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # flake-parts
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
