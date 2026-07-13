d8bd990b6e2a9a9893b01ea9afd9f1e4edfd746c
machtendo <machjas@outlook.com>
2026-07-12 22:55:35 -0500

    specialArgs to inherit inputs. troubleshooting.

--- modules/hosts/mediarr/default.nix
+++ modules/hosts/mediarr/default.nix
@@ -5,6 +5,7 @@
 { self, inputs, config, sops-nix, nixflix, ... }: {
   flake.nixosConfigurations.mediarr = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
     modules = [
 
       # Host Configuration
