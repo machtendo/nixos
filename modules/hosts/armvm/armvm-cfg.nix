e56dde60f87fe3128c9ae363e57099436e0e999f
machtendo <machjas@outlook.com>
2026-07-15 16:25:38 -0500

    modules split: armvm, armvm-cfg, armvm-hw

---
+++ modules/hosts/armvm/armvm-cfg.nix
@@ -0,0 +1,65 @@
+#---------------------------------------------------------------------------------------------------
+# Host Configuration: armvm
+#---------------------------------------------------------------------------------------------------
+
+{ self, inputs, ... }: {
+
+  flake.nixosModules.armvm-cfg = { pkgs, lib, ... }: {
+    imports = [
+      self.nixosModules.armvm-hw
+    ];
+
+    # Flakes  ------------------------------------
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Boot ---------------------------------------
+
+    #boot.kernelParams = [ "reboot=acpi" ];
+
+    # Bootloader ---------------------------------
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
+
+    # Networking ---------------------------------
+
+    # Enable Networking
+    networking.networkmanager.enable = true;
+
+    # Hostname
+    networking.hostName = "armvm";
+
+    # Network Proxy
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Firewall
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    # Packages  ----------------------------------
+
+    # System
+    environment.systemPackages = with pkgs; [
+     # ...
+    ];
+
+    # Services -----------------------------------
+
+    services.openssh.enable = true;     # OpenSSH Server
+
+    # System State Version -----------------------
+    system.stateVersion = "26.05";
+
+    #--------------------------------------------#
+    #--------------------------------------------#
+    #--------------------------------------------#
+
+  };
+}
+
+#---------------------------------------------------------------------------------------------------
+# End
+#---------------------------------------------------------------------------------------------------
--- modules/hosts/armvm/armvm.nix
+++ modules/hosts/armvm/armvm.nix
@@ -1,5 +1,5 @@
 #---------------------------------------------------------------------------------------------------
-# Host Configuration: armvm
+# Host Definition: armvm
 #---------------------------------------------------------------------------------------------------
 
 { self, inputs, ... }: {
@@ -7,66 +7,11 @@
   flake.nixosConfigurations.armvm = inputs.nixpkgs.lib.nixosSystem {
     system = "aarch64-linux";
     modules = with self.nixosModules; [
-      armvm         # Configuration - Host: armvm
+      armvm-cfg     # Configuration - Host: armvm
       armvm-hw      # Configuration - Hardware: armvm
       user-nix      # Configuration - User: nix
       core          # Configuration - Core: All Devices
     ];
-  };
-
-  flake.nixosModules.armvm = { pkgs, lib, ... }: {
-    imports = [
-      # ...
-    ];
-
-    # Flakes  ------------------------------------
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    # Boot ---------------------------------------
-
-    #boot.kernelParams = [ "reboot=acpi" ];
-
-    # Bootloader ---------------------------------
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda";
-    boot.loader.grub.useOSProber = true;
-
-    # Networking ---------------------------------
-
-    # Enable Networking
-    networking.networkmanager.enable = true;
-
-    # Hostname
-    networking.hostName = "armvm";
-
-    # Network Proxy
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-    # Firewall
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
-
-    # Packages  ----------------------------------
-
-    # System
-    environment.systemPackages = with pkgs; [
-     # ...
-    ];
-
-    # Services -----------------------------------
-
-    services.openssh.enable = true;     # OpenSSH Server
-
-    # System State Version -----------------------
-    system.stateVersion = "26.05";
-
-    #--------------------------------------------#
-    #--------------------------------------------#
-    #--------------------------------------------#
-
   };
 }
 
