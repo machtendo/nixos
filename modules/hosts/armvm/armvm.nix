13353a0e46ce1e45f487d89f3d348f120caa0a1b
machtendo <machjas@outlook.com>
2026-07-15 16:53:16 -0500

    troubleshoot revert to previous configuration, it's not taking

--- modules/hosts/armvm/armvm.nix
+++ modules/hosts/armvm/armvm.nix
@@ -5,7 +5,6 @@
 { self, inputs, ... }: {
 
   flake.nixosConfigurations.armvm = inputs.nixpkgs.lib.nixosSystem {
-    system = "aarch64-linux";
     modules = with self.nixosModules; [
       armvm-cfg     # Configuration - Host: armvm
       #armvm-hw      # Configuration - Hardware: armvm
