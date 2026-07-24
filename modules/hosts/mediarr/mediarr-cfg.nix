908ed6fbd2e62462417ba4de411fed9d79959578
machtendo <machjas@outlook.com>
2026-07-24 18:42:08 -0500

    switch back to grub bootloader

--- modules/hosts/mediarr/mediarr-cfg.nix
+++ modules/hosts/mediarr/mediarr-cfg.nix
@@ -13,13 +13,13 @@
     system.stateVersion = "25.11";
 
     # Bootloader
-    #boot.loader.grub.enable = true;
-    #boot.loader.grub.device = "/dev/sda";
-    #boot.loader.grub.useOSProber = true;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
 
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.efi.efiSysMountPoint = "/boot";
+    #boot.loader.systemd-boot.enable = true;
+    #boot.loader.efi.canTouchEfiVariables = true;
+    #boot.loader.efi.efiSysMountPoint = "/boot";
 
     # Networking #-------------------------------#
     # System-specific Network Settings
