faf539387a16ac3136be6acdee8d96a771ede541
machtendo <machjas@outlook.com>
2026-07-24 23:35:52 -0500

    adding llm config - hermes agent

---
+++ modules/features/llm.nix
@@ -0,0 +1,40 @@
+#--------------------------------------------------------------------------------------------------#
+# Configuration: Core System - All Devices
+#--------------------------------------------------------------------------------------------------#
+
+{ self, inputs, ... }: {
+
+  flake.nixosModules.llm = { pkgs, lib, ... }: {
+
+    imports = [
+      hermes-agent.nixosModules.default
+      # ...
+    ];
+
+    # Hermes
+    services.hermes-agent = {
+      enable = true;
+      #configFile = /etc/hermes/config.yaml;
+      settings = {
+        model.default         = "anthropic/claude-sonnet-4";
+        environmentFiles      = [ config.sops.secrets."hermes-env".path ];
+        addToSystemPackages   = true;
+        toolsets              = [ "all" ];
+        terminal              = { backend = "local"; timeout = 180; };
+
+        # Personality
+        display   = { compact = false; personality = "kawaii"; };
+        memory    = { memory_enabled = true; user_profile_enabled = true; };
+      };
+    };
+
+    #--------------------------------------------#
+    #--------------------------------------------#
+    #--------------------------------------------#
+
+  };
+}
+
+#--------------------------------------------------------------------------------------------------#
+# End
+#--------------------------------------------------------------------------------------------------#
