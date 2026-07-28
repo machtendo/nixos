1bf40cface0ceb5253437a7d1e9f020e3cfa37c8
machtendo <machjas@outlook.com>
2026-07-28 01:21:43 -0500

    remove nixConfig option from module - wrong place - belongs in base flake.nix

--- modules/features/llm.nix
+++ modules/features/llm.nix
@@ -9,11 +9,6 @@
      # ...
     ];
 
-    nixConfig = {
-      extra-substituters          = [ "https://cache.numtide.com" ];
-      extra-trusted-public-keys   = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
-    };
-
     environment = {
       systemPackages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
         hermes-agent
