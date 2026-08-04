dbd310af1ed31786448af8cc5c9914d230f3cfcd
machtendo <machjas@outlook.com>
2026-08-04 17:00:03 -0500

    create a module with an overlay to define overrides for unstable packages

---
+++ modules/overlays/unstable.nix
@@ -0,0 +1,47 @@
+#--------------------------------------------------------------------------------------------------
+# Module Configuration: Overlay - Unstable Packages (nixpkgs-unstable)
+#--------------------------------------------------------------------------------------------------
+
+{ self, inputs, ... }: {
+
+  flake.nixopsModules.overlay-unstablePackages = {pkgs, lib, ... }: {
+
+    imports = [
+      # ...
+    ];
+
+    # Option Definition
+    options.unstablePackages = {
+      default       = [];
+      type          = types.list;
+      description   = "List of packages to source from nixpkgs-unstable.";
+    };
+
+    # Overlay Definition
+    nixpkgs.overlays = [(
+      final: prev:
+
+      let
+        unstable      = import inputs.nixpkgs-unstable {
+          system      = pkgs.system;
+          config      = {};
+        };
+
+        # List Definition
+        overrides = builtins.listToAttr (map (name: {
+          name        = name;
+          value       = unstable.${name};
+        }) config.unstablePackages);
+
+      in
+        # Merge List into Package List
+        overrides
+    )];
+
+  };
+
+}
+
+#---------------------------------------------------------------------------------------------------
+# End
+#---------------------------------------------------------------------------------------------------
