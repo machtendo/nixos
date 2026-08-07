2f13ddff45207fe7a9bc4fdd4c6fdd227d69d65c
machtendo <machjas@outlook.com>
2026-08-06 21:09:07 -0500

    let's try to get a DE going

--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -10,20 +10,6 @@
       inputs.noctalia.nixosModules.default
     ];
 
-    # Cachix ------------------------------------#
-    # Package Cache for hyprland
-    #--------------------------------------------#
-
-    #nix.settings = {
-    #  substituters                = [ "https://hyprland.cachix.org" ];
-    #  trusted-substituters        = [ "https://hyprland.cachix.org" ];
-    #  extra-substituters          = [ "https://noctalia.cachix.org" ];
-    #  trusted-public-keys         = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-    #  extra-trusted-public-keys   = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-    #  trusted-users               = [ "root" "@wheel" ];
-    #  # ...
-    #};
-
     # Desktop Environment -----------------------#
     # Hyprland - Wayland Compositor
     #--------------------------------------------#
