01dd9f897fde32619b29df072a5a704299019e6b
machtendo <machjas@outlook.com>
2026-08-08 01:45:28 -0500

    reformatting

--- modules/features/nixflix.nix
+++ modules/features/nixflix.nix
@@ -5,8 +5,8 @@
 { self, inputs, config, sops-nix, nixflix, ... }: {
 
   flake.nixosModules.nixflix = { pkgs, lib, ... }: {
-
     specialArgs = { inherit inputs; };
+
     imports = [
       #...
     ];
@@ -15,32 +15,38 @@
     # Importing secrets
     #--------------------------------------------#
 
-    sops.defaultSopsFile = ../../../../secrets/nixflix.yaml;
-    sops.defaultSopsFormat = "yaml";
-    sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    sops.secrets = {
-      "sonarr/api_key" = {};
-      "sonarr/password" = {};
-      "radarr/api_key" = {};
-      "radarr/password" = {};
-      "lidarr/api_key" = {};
-      "lidarr/password" = {};
-      "prowlarr/api_key" = {};
-      "prowlarr/password" = {};
-      "indexer-api-keys/DrunkenSlug" = {};
-      "indexer-api-keys/NZBFinder" = {};
-      "indexer-api-keys/NzbPlanet" = {};
-      "jellyfin/alice_password" = {};
-      "seerr/api_key" = {};
-      "wireguard/conf" = {};
-      "sabnzbd/api_key" = {};
-      "sabnzbd/nzb_key" = {};
-      "sabnzbd/username" = { };
-      "sabnzbd/password" = { };
-      "usenet/eweka/username" = {};
-      "usenet/eweka/password" = {};
-      "usenet/newsgroupdirect/username" = {};
-      "usenet/newsgroupdirect/password" = {};
+    sops = {
+      defaultSopsFile       = ../../../../secrets/nixflix.yaml;
+      defaultSopsFormat     = "yaml";
+
+      age = {
+        sshKeyPaths         = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      };
+
+      secrets = {
+        "sonarr/api_key"                  = {};
+        "sonarr/password"                 = {};
+        "radarr/api_key"                  = {};
+        "radarr/password"                 = {};
+        "lidarr/api_key"                  = {};
+        "lidarr/password"                 = {};
+        "prowlarr/api_key"                = {};
+        "prowlarr/password"               = {};
+        "indexer-api-keys/DrunkenSlug"    = {};
+        "indexer-api-keys/NZBFinder"      = {};
+        "indexer-api-keys/NzbPlanet"      = {};
+        "jellyfin/alice_password"         = {};
+        "seerr/api_key"                   = {};
+        "wireguard/conf"                  = {};
+        "sabnzbd/api_key"                 = {};
+        "sabnzbd/nzb_key"                 = {};
+        "sabnzbd/username"                = {};
+        "sabnzbd/password"                = {};
+        "usenet/eweka/username"           = {};
+        "usenet/eweka/password"           = {};
+        "usenet/newsgroupdirect/username" = {};
+        "usenet/newsgroupdirect/password" = {};
+      };
     };
 
     # Nixflix #----------------------------------#
@@ -48,18 +54,18 @@
     #--------------------------------------------#
 
     nixflix = {
-      enable = true;
-      mediaDir = "/data/media";
-      stateDir = "/data/.state";
-      #mediaUsers = ["myuser"];
+      enable      = true;
+      mediaDir    = "/data/media";
+      stateDir    = "/data/.state";
+      mediaUsers  = ["nixflix"];
 
       # Theme Park #-----------------------------#
       # Unified Appearance
       #------------------------------------------#
 
       theme = {
-        enable = true;
-        name = "overseerr";
+        enable  = true;
+        name    = "overseerr";
       };
 
       # Nginx #----------------------------------#
@@ -67,25 +73,36 @@
       #------------------------------------------#
 
       nginx = {
-        enable = true;
-        addHostsEntries = true; # Disable this if you have your own DNS configuration
+        enable            = true;
+        addHostsEntries   = true; # Disable this if you have your own DNS configuration
       };
 
       # PostgreSQL #-----------------------------#
       # Shared database backend
       #------------------------------------------#
 
-      postgres.enable = true;
+      postgres = {
+        enable = true;
+      };
 
       # Sonarr #---------------------------------#
       # Automated Media Management (TV)
       #------------------------------------------#
 
       sonarr = {
-        enable = true;
+        enable        = true;
         config = {
-          apiKey._secret = config.sops.secrets."sonarr/api_key".path;
-          hostConfig.password._secret = config.sops.secrets."sonarr/password".path;
+
+          apiKey = {
+            _secret   = config.sops.secrets."sonarr/api_key".path;
+          };
+
+          hostConfig = {
+            password = {
+              _secret = config.sops.secrets."sonarr/password".path;
+            };
+          };
+
         };
       };
 
@@ -95,9 +112,19 @@
 
       radarr = {
         enable = true;
+
         config = {
-          apiKey._secret = config.sops.secrets."radarr/api_key".path;
-          hostConfig.password._secret = config.sops.secrets."radarr/password".path;
+
+          apiKey = {
+            _secret   = config.sops.secrets."radarr/api_key".path;
+          };
+
+          hostConfig = {
+            password = {
+              _secret = config.sops.secrets."radarr/password".path;
+            };
+          };
+
         };
       };
 
@@ -107,9 +134,18 @@
 
       lidarr = {
         enable = true;
+
         config = {
-          apiKey._secret = config.sops.secrets."lidarr/api_key".path;
-          hostConfig.password._secret = config.sops.secrets."lidarr/password".path;
+
+          apiKey = {
+            _secret   = config.sops.secrets."lidarr/api_key".path;
+          };
+
+          hostConfig = {
+            password = {
+              _secret = config.sops.secrets."lidarr/password".path;
+            };
+          };
         };
       };
 
@@ -128,29 +164,38 @@
 
       prowlarr = {
         enable = true;
+
         config = {
-          apiKey._secret = config.sops.secrets."prowlarr/api_key".path;
-          hostConfig.password._secret = config.sops.secrets."prowlarr/password".path;
-          indexers = [
+          apiKey = {
+            _secret = config.sops.secrets."prowlarr/api_key".path;
+          };
 
-            # Usenet Indexers
+          hostConfig = {
+            password = {
+              _secret = config.sops.secrets."prowlarr/password".path;
+            };
 
-            #{
-            #  name = "DrunkenSlug";
-            #  apiKey._secret = config.sops.secrets."indexer-api-keys/DrunkenSlug".path;
-            #}
+            indexers = [
 
-            #{
-            #  name = "NZBFinder";
-            #  apiKey._secret = config.sops.secrets."indexer-api-keys/NZBFinder".path;
-            #}
+              # Usenet Indexers
 
-            #{
-            #  name = "NzbPlanet";
-            #  apiKey._secret = config.sops.secrets."indexer-api-keys/NzbPlanet".path;
-            #}
+              #{
+              #  name = "DrunkenSlug";
+              #  apiKey._secret = config.sops.secrets."indexer-api-keys/DrunkenSlug".path;
+              #}
 
-          ];
+              #{
+              #  name = "NZBFinder";
+              #  apiKey._secret = config.sops.secrets."indexer-api-keys/NZBFinder".path;
+              #}
+
+              #{
+              #  name = "NzbPlanet";
+              #  apiKey._secret = config.sops.secrets."indexer-api-keys/NzbPlanet".path;
+              #}
+
+            ];
+          };
         };
       };
 
@@ -162,11 +207,24 @@
         enable = false;
 
         settings = {
+
           misc = {
-            api_key._secret = config.sops.secrets."sabnzbd/api_key".path;
-            nzb_key._secret = config.sops.secrets."sabnzbd/nzb_key".path;
-            username._secret = config.sops.secrets."sabnzbd/username".path;
-            password._secret = config.sops.secrets."sabnzbd/password".path;
+            api_key = {
+              _secret = config.sops.secrets."sabnzbd/api_key".path;
+            };
+
+            nzb_key = {
+              _secret = config.sops.secrets."sabnzbd/nzb_key".path;
+            };
+
+            username = {
+              _secret = config.sops.secrets."sabnzbd/username".path;
+            };
+
+            password = {
+              _secret = config.sops.secrets."sabnzbd/password".path;
+            };
+
           };
 
           servers = [
@@ -206,12 +264,22 @@
 
       jellyfin = {
         enable = true;
-        apiKey._secret = config.sops.secrets."jellyfin/api_key".path;
+
+      apiKey = {
+        _secret = config.sops.secrets."jellyfin/api_key".path;
+
         users = {
           admin = {
             mutable = false;
-            policy.isAdministrator = true;
-            password._secret = config.sops.secrets."jellyfin/alice_password".path;
+
+            policy = {
+              isAdministrator = true;
+            };
+
+            password = {
+              _secret = config.sops.secrets."jellyfin/alice_password".path;
+            };
+
           };
         };
       };
@@ -222,7 +290,11 @@
 
       seerr = {
         enable = true;
-        apiKey._secret = config.sops.secrets."seerr/api_key".path;
+
+        apiKey = {
+          _secret = config.sops.secrets."seerr/api_key".path;
+        };
+
       };
 
       # Wireguard #------------------------------#
@@ -230,9 +302,9 @@
       #------------------------------------------#
 
       vpn = {
-        enable = true;
-        wgConfFile = config.sops.secrets."wireguard/conf".path;
-        accessibleFrom = [ "192.168.86.0/24" ];
+        enable          = true;
+        wgConfFile      = config.sops.secrets."wireguard/conf".path;
+        accessibleFrom  = [ "192.168.86.0/24" ];
       };
 
       #------------------------------------------#
