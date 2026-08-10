ef89c51a668fd7ab331a66c68af8c84288dc2882
machtendo <machjas@outlook.com>
2026-08-09 20:00:36 -0500

    adding 'systems' for perSystem build

--- flake.nix
+++ flake.nix
@@ -138,7 +138,15 @@
 #---------------------------------------------------------------------------------------------------
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake
-    { inherit inputs; }
+    {
+      inherit inputs;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    };
 
     (inputs.import-tree ./modules);
 
