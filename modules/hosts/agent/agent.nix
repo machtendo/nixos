230728087bf42c3f05c2e85d0d99b74289d8bbed
machtendo <machjas@outlook.com>
2026-08-14 16:08:21 -0500

    disable desktop module for agent host

--- modules/hosts/agent/agent.nix
+++ modules/hosts/agent/agent.nix
@@ -12,7 +12,7 @@
       agent-hw      # Configuration - Hardware: armvm
       user-nix      # Configuration - User: nix
       core          # Configuration - Core: All Devices
-      desktop       # Configuration - Desktop Environment: Hyprland, Noctalia
+#      desktop       # Configuration - Desktop Environment: Hyprland, Noctalia
       llm           # Configuration - LLM - Hermes Agent
 
       inputs.sops-nix.nixosModules.sops     # Security - Tools: sops-nix
