489fed4858869406ede9f9260f316faa2deb672a
machtendo <machjas@outlook.com>
2026-08-16 22:46:31 -0500

    troubleshoot build

--- modules/features/llm.nix
+++ modules/features/llm.nix
@@ -76,7 +76,7 @@
           environment         = {};
 
           # Secrets ---------
-          environmentFiles    = [ inputs.sops.secrets."hermes-env".path ];
+          environmentFiles    = [ config.sops.secrets."hermes-env".path ];
 
           # Container --------------------------------
           # Run in a Container (optional)
