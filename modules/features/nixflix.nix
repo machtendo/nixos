20d817055a61b833bee2825d37535eda671b7a80
machtendo <machjas@outlook.com>
2026-09-08 00:36:33 -0500

    qbittorrent configuration, adding kiri's systemd service for port rotation in ProtonVPN

--- modules/features/nixflix.nix
+++ modules/features/nixflix.nix
@@ -50,6 +50,7 @@
         "sabnzbd/nzb_key"                 = {};
         "sabnzbd/username"                = {};
         "sabnzbd/password"                = {};
+        "qbittorrent/password"            = {};
         "usenet/eweka/username"           = {};
         "usenet/eweka/password"           = {};
         "usenet/newsgroupdirect/username" = {};
@@ -250,21 +251,27 @@
             {
              enable = false;
              name = "DrunkenSlug";
-             apiKey._secret    = config.sops.secrets."indexer-api-keys/DrunkenSlug".path;
+             apiKey = {
+               _secret        = config.sops.secrets."indexer-api-keys/DrunkenSlug".path;
+             };
             }
 
             # NZBFinder
             {
              enable = false;
              name = "NZBFinder";
-             apiKey._secret    = config.sops.secrets."indexer-api-keys/NZBFinder".path;
+             apiKey = {
+               _secret         = config.sops.secrets."indexer-api-keys/NZBFinder".path;
+             };
             }
 
             #NzbPlanet
             {
              enable = false;
              name = "NzbPlanet";
-             apiKey._secret    = config.sops.secrets."indexer-api-keys/NzbPlanet".path;
+             apiKey = {
+               _secret        = config.sops.secrets."indexer-api-keys/NzbPlanet".path;
+             };
             }
 
             # Torrent Indexers -------------------
@@ -285,11 +292,112 @@
       # Bittorrent Client
       #-------------------------------------------
 
-      qbittorrent = {
-        enable = true;
-
+      torrentClients = {
+        qbittorrent = {
+          enable = true;
+          subdomain = "torrent";
+          password = {
+            _secret = config.sops.secrets."qbittorrent/password".path;
+          };
+          serverConfig = {
+            LegalNotice = {
+              Accepted = true;
+            };
+            BitTorrent = {
+                Session = {
+                  AddTorrentStopped = false;
+                  Port = 45500;
+                  QueueingSystemEnabled = true;
+                  SSL = {
+                    Port = 32380;
+                  };
+                  ReannounceWhenAddressChanged = true;
+                };
+            };
+            Preferences = {
+              WebUI = {
+                Username = "torrent";
+                Password_PBKDF2 = "";
+              };
+              General = {
+                Locale = "en";
+              };
+            };
+          };
+        };
       };
 
+      systemd.services.protonvpn-port-forward = lib.mkIf (config.nixflix.vpn.enable && qbCfg.vpn.enable) {
+        description = "ProtonVPN port forwarding for qBittorrent";
+        after = [
+          "${config.systemd.services.qbittorrent.vpnConfinement.vpnNamespace}.service"
+          "qbittorrent.service"
+        ];
+        requires = [
+          "${config.systemd.services.qbittorrent.vpnConfinement.vpnNamespace}.service"
+          "qbittorrent.service"
+        ];
+        wantedBy = [ "multi-user.target" ];
+
+        path = [
+          pkgs.curl
+          pkgs.jq
+          pkgs.libnatpmp
+          pkgs.iproute2
+          pkgs.gawk
+        ];
+
+        serviceConfig = {
+          Type = "simple";
+          Restart = "on-failure";
+          RestartSec = "5s";
+          ExecStart =
+            let
+              ns = config.systemd.services.qbittorrent.vpnConfinement.vpnNamespace;
+              qbHost = "http://${qbCfg.serverConfig.Preferences.WebUI.Address}:${toString config.services.qbittorrent.webuiPort}";
+              qbUser = qbCfg.serverConfig.Preferences.WebUI.Username;
+              qbPassFile = config.sops.secrets."qbittorrent/password".path;
+            in
+            pkgs.writeShellScript "protonvpn-port-forward" ''
+              QB_HOST="${qbHost}"
+              QB_USER="${qbUser}"
+              QB_PASS=$(cat ${qbPassFile})
+
+              QB_COOKIE=$(curl -s -c - --data "username=$QB_USER&password=$QB_PASS" \
+                "$QB_HOST/api/v2/auth/login" | grep SID | awk '{print $NF}')
+
+              CURRENT_PORT=$(curl -s -b "SID=$QB_COOKIE" \
+                "$QB_HOST/api/v2/app/preferences" | jq '.listen_port')
+
+              while true; do
+                UDP_OUT=$(ip netns exec ${ns} natpmpc -a 1 0 udp 60 -g 10.2.0.1)
+                ip netns exec ${ns} natpmpc -a 1 0 tcp 60 -g 10.2.0.1
+
+                PORT=$(echo "$UDP_OUT" | grep "Mapped public port" | awk '{print $4}')
+
+                if [ -z "$PORT" ]; then
+                  echo "Failed to get port, is the tunnel up?"
+                  sleep 5
+                  continue
+                fi
+
+                if [ "$PORT" != "$CURRENT_PORT" ]; then
+                  echo "Port changed: $CURRENT_PORT -> $PORT, updating qBittorrent..."
+                  curl -s -b "SID=$QB_COOKIE" \
+                    --data "json={\"listen_port\":$PORT,\"random_port\":false}" \
+                    "$QB_HOST/api/v2/app/setPreferences"
+                  curl -s -b "SID=$QB_COOKIE" \
+                    --data "hashes=all" \
+                    "$QB_HOST/api/v2/torrents/reannounce"
+                  CURRENT_PORT=$PORT
+                fi
+
+                sleep 45
+              done
+            '';
+          };
+        };
+      };
       # SABnzbd ----------------------------------
       # Automation of Usenet Transfers
       #-------------------------------------------
