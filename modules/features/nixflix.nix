985085aa5642e17213da382080f5aca225b18e67
machtendo <machjas@outlook.com>
2026-09-08 00:47:45 -0500

    nesting sabnzbd under usenetClients

--- modules/features/nixflix.nix
+++ modules/features/nixflix.nix
@@ -402,52 +402,54 @@
       # Automation of Usenet Transfers
       #-------------------------------------------
 
-      sabnzbd = {
-        enable                  = false;
-        subdomain               = "nzb";
-        settings = {
-          misc = {
-            api_key = {
-              _secret           = config.sops.secrets."sabnzbd/api_key".path;
-            };
-            nzb_key = {
-              _secret           = config.sops.secrets."sabnzbd/nzb_key".path;
-            };
-            username = {
-              _secret           = config.sops.secrets."sabnzbd/username".path;
-            };
-            password = {
-              _secret           = config.sops.secrets."sabnzbd/password".path;
+      usenetClients = {
+        sabnzbd = {
+          enable                  = false;
+          subdomain               = "nzb";
+          settings = {
+            misc = {
+              api_key = {
+                _secret           = config.sops.secrets."sabnzbd/api_key".path;
+              };
+              nzb_key = {
+                _secret           = config.sops.secrets."sabnzbd/nzb_key".path;
+              };
+              username = {
+                _secret           = config.sops.secrets."sabnzbd/username".path;
+              };
+              password = {
+                _secret           = config.sops.secrets."sabnzbd/password".path;
+              };
+
             };
 
+          servers = [
+          #
+          #   {
+          #    name               = "Eweka";
+          #    host               = "sslreader.eweka.nl";
+          #    port               = 563;
+          #    username._secret   = config.sops.secrets."usenet/eweka/username".path;
+          #    password._secret   = config.sops.secrets."usenet/eweka/password".path;
+          #    connections        = 20;
+          #    ssl                = true;
+          #    priority           = 0;
+          #   }
+          #
+          #   {
+          #    name               = "NewsgroupDirect";
+          #    host               = "news.newsgroupdirect.com";
+          #    port               = 563;
+          #    username._secret   = config.sops.secrets."usenet/newsgroupdirect/username".path;
+          #    password._secret   = config.sops.secrets."usenet/newsgroupdirect/password".path;
+          #    connections        = 10;
+          #    ssl                = true;
+          #    priority           = 1;
+          #    optional           = true;
+          #    backup             = true;
+          #   }
+            ];
           };
-
-        servers = [
-        #
-        #   {
-        #    name               = "Eweka";
-        #    host               = "sslreader.eweka.nl";
-        #    port               = 563;
-        #    username._secret   = config.sops.secrets."usenet/eweka/username".path;
-        #    password._secret   = config.sops.secrets."usenet/eweka/password".path;
-        #    connections        = 20;
-        #    ssl                = true;
-        #    priority           = 0;
-        #   }
-        #
-        #   {
-        #    name               = "NewsgroupDirect";
-        #    host               = "news.newsgroupdirect.com";
-        #    port               = 563;
-        #    username._secret   = config.sops.secrets."usenet/newsgroupdirect/username".path;
-        #    password._secret   = config.sops.secrets."usenet/newsgroupdirect/password".path;
-        #    connections        = 10;
-        #    ssl                = true;
-        #    priority           = 1;
-        #    optional           = true;
-        #    backup             = true;
-        #   }
-          ];
         };
       };
 
