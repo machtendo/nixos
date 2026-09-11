8b69c48df8099b4063aa64432ed0d8d63b896a1d
machtendo <machjas@outlook.com>
2026-09-11 15:52:14 -0500

    minor refactor - adding 'services' folder, rename llm.nix to hermes.nix, and moved it from 'features' to 'applications'

--- modules/hosts/agent/agent.nix
+++ modules/hosts/agent/agent.nix
@@ -12,11 +12,10 @@
       agent-hw      # Configuration - Hardware: armvm
       user-nix      # Configuration - User: nix
       core          # Configuration - Core: All Devices
-      #desktop       # Configuration - Desktop Environment: Hyprland, Noctalia
-      llm           # Configuration - LLM - Hermes Agent
+      #desktop      # Configuration - Desktop Environment: Hyprland, Noctalia
+      hermes        # Configuration - LLM - Hermes Agent
 
       inputs.sops-nix.nixosModules.sops     # Security - Tools: sops-nix
-
     ];
   };
 }
